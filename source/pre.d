6fecc677c76301accb7cacda2ca876e6bc360125
Max Haughton <maxhaton@gmail.com>
2019-07-06 02:15:40 +0100

    Preprocessor work

--- source/pre.d
+++ source/pre.d
@@ -1,10 +1,47 @@
 ///Hacky preprocess
 module pre;
 ///Preprocess some lines
-string[] preProcess(string input) {
+string[] preProcess(string input)
+{
     import std.string;
-    //Do nothing if no preprocessor tokens found
-    if(input.indexOf('#') < 0)
-        return input.splitLines;
-    return input.splitLines;
+
+    /*
+        Even if there are no # directives present we have to 
+        join \ lines.
+    */
+    auto lines = input.splitLines;
+    lines = lines.joinBackLines;
+    return lines;
+}
+///Join lines containng \ together
+string[] joinBackLines(string[] lines)
+{
+    foreach_reverse (i, ref line; lines)
+    {
+        //Not a continued line present.
+        //if(line.length == 0) break;
+        if (line.length)
+        {
+
+            if (line[$ - 1] == '\\')
+            {
+                line = line[0 .. $ - 1];
+                line ~= lines[i + 1];
+                lines[i + 1] = "";
+            }
+        }
+    }
+    return lines;
+}
+///Get the contents of a file
+template importer(string filePath)
+    if(__traits(compiles, import(filePath)))
+{
+    string importer = import(filePath);
+}
+///Handle preprocessor directives.
+string[] directive(string[] input)
+{
+    string[] defined;
+    return input;
 }
